8e659763d65bbff8c4ad7939249ef42f08c9173a
z3m1ya <96194885+z3m1ya@users.noreply.github.com>
2022-03-01 12:20:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,18 +1,18 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch2(int* arr, int l, int r, int value)
-{
-	if (r >= l) {
-		int m = l + (r - l) / 2;
-		if (arr[m] == value)
-			return cbinsearch2(arr, l, m - 1, value) + cbinsearch2(arr, m + 1, r, value) + 1;
-		if (arr[m] > value)
-			return cbinsearch2(arr, l, m - 1, value);
-		return cbinsearch2(arr, m + 1, r, value);
-	}
-	return 0;
+int cbinsearch2(int* arr, int l, int r, int value) {
+    if (r >= l) {
+        int m = l + (r - l) / 2;
+        if (arr[m] == value)
+            return cbinsearch2(arr, l, m - 1, value) 
+            + cbinsearch2(arr, m + 1, r, value) + 1;
+        if (arr[m] > value)
+            return cbinsearch2(arr, l, m - 1, value);
+        return cbinsearch2(arr, m + 1, r, value);
+    }
+    return 0;
 }
 
 int cbinsearch(int* arr, int size, int value) {
-	return cbinsearch2(arr, 0, size, value);
+    return cbinsearch2(arr, 0, size, value);
 }
