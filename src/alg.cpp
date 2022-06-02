37ab41b660ae3fb85d754d6a0ed8fda4fb68068e
z3m1ya <96194885+z3m1ya@users.noreply.github.com>
2022-06-02 16:04:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,12 +2,12 @@
 
 int cbinsearch2(int* a, int l, int r, int v) {
     if (r >= l) {
-        int m = l + (r - l) / 2;
+        int m = l + (r - l) / 2 - 1;
         if (arr[m] == v)
-            return cbinsearch2(a, l, m - 1, v) + cbinsearch2(a, m + 1, r, v) + 1;
+            return cbinsearch2(a, l, m, v) + cbinsearch2(a, m + 2, r, v) + 1;
         if (arr[m] > value)
-            return cbinsearch2(a, l, m - 1, v);
-        return cbinsearch2(a, m + 1, r, v);
+            return cbinsearch2(a, l, m, v);
+        return cbinsearch2(a, m + 2, r, v);
     }
     return 0;
 }
